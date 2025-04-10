35ac98e555d5cf19600ae71b2085518a2769edc4
Przemek Joniec <joniecprzemek098@gmail.com>
2025-04-10 21:48:17 +0200

    algorytmy zamiany

---
+++ Algorytmy/zamiana z 10 na dowolny system.py
@@ -0,0 +1,24 @@
+def zamianaz10z2(n):
+    wynik = ""
+    while n > 0:
+        wynik = str(n % 2) + wynik
+        n = n // 2
+    return wynik
+
+def zamianaz10z7(n):
+    wynik = ""
+    while n > 0:
+        wynik = str(n % 7) + wynik
+        n = n // 7
+    return wynik
+
+def zamianaz10z3(n):
+    wynik = ""
+    while n > 0:
+        wynik = str(n % 3) + wynik
+        n = n // 3
+    return wynik
+
+print(zamianaz10z2(45))
+print(zamianaz10z7(45))
+print(zamianaz10z3(45))
